726c2d617c092f80d02de831e0b71084cdcaa9fe
umut <umut_baykan@hotmail.com>
2023-05-24 11:05:03 +0100

    checking rows for wins method

--- src/board.js
+++ src/board.js
@@ -1,14 +1,31 @@
 class Board {
   constructor (cp) {
     this.board = [
-      [cp, cp, cp],
-      [cp, cp, cp],
-      [cp, cp, cp]
+      [cp.d, cp.d, cp.d],
+      [cp.d, cp.d, cp.d],
+      [cp.d, cp.d, cp.d]
     ]
+    this.cp = cp
   }
 
   mark = ([row, column], symbol) => {
     this.board[row][column] = symbol
+  }
+
+  checkWins = () => {
+   
+  }
+
+  checkRowsForWin = () => {
+    for (let i = 0; i <= 2; i++) {
+      if (
+        this.board[i][0] !== this.cp.d &&
+        this.board[i][0] === this.board[i][1] &&
+        this.board[i][1] === this.board[i][2]) {
+        return true
+      }
+    }
+    return false
   }
 }
 
--- tests/board.test.js
+++ tests/board.test.js
@@ -1,8 +1,14 @@
 const Board = require('../src/board')
 
 describe('board class', () => {
+  let newBoard
+  const cp = { d: ' ', x: 'X', o: 'O' }
+
+  beforeEach(() => {
+    newBoard = new Board(cp)
+  })
+
   test('it initializes with an empty matrix', () => {
-    const newBoard = new Board(' ')
     expect(newBoard.board).toEqual([
       [' ', ' ', ' '],
       [' ', ' ', ' '],
@@ -11,12 +17,27 @@ describe('board class', () => {
   })
 
   test('can change the component symbol', () => {
-    const newBoard = new Board(' ')
-    newBoard.mark([0, 2], 'X')
+    newBoard.mark([0, 2], cp.x)
     expect(newBoard.board).toEqual([
       [' ', ' ', 'X'],
       [' ', ' ', ' '],
       [' ', ' ', ' ']
     ])
+  })
+
+  describe('checkRowsForWin method', () => {
+    test('returns true if all row elements are matching with x', () => {
+      newBoard.board[0] = [cp.x, cp.x, cp.x]
+      expect(newBoard.checkRowsForWin()).toEqual(true)
+    })
+
+    test('returns true if all row elements are matching with o', () => {
+      newBoard.board[2] = [cp.o, cp.o, cp.o]
+      expect(newBoard.checkRowsForWin()).toEqual(true)
+    })
+
+    test('returns false if row is in default state', () => {
+      expect(newBoard.checkRowsForWin()).toEqual(false)
+    })
   })
 })
