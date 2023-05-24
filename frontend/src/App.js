ae4a01c9130a253f8932b716ac23981a50115eb9
umut <umut_baykan@hotmail.com>
2023-05-24 16:25:29 +0100

    game can be restarted

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import Board from './components/board/Board'
 import './App.css';
 
 function App() {
+
+  const [xNext, setxNext] = useState(true)
+  const [gameOngoing, setGameOngoing] = useState(true)
+
+  const whoseTurnIsIt = () => {
+    if (xNext) {
+      return 'X'    
+    } else {
+      return 'O'
+    }
+  }
+
+  const changeTurn = () => {
+    setxNext(!xNext)
+  }
+
+  const gameWon = () => {
+    changeTurn()
+    setGameOngoing(!gameOngoing)
+  }
+
+  const restartGame = () => {
+    setGameOngoing(true)
+    setxNext(true)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {gameOngoing ? 
+      <>
+      <h4>It is currently {whoseTurnIsIt()} turn</h4>
+      <Board changeTurn={changeTurn} xNext={xNext} gameWon={gameWon}/> 
+      </>
+      :
+      <>
+      <h2>{whoseTurnIsIt()} won!</h2>
+      <button onClick={restartGame}>Replay?</button>
+      </>
+    }
+    </>
   );
 }
 
