caf5b9ebc3c1902081a777b75c04bd67dd725290
umut <umut_baykan@hotmail.com>
2023-05-26 18:20:21 +0100

    game win and draw logic implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,45 @@
-import logo from "./logo.svg";
 import { useEffect, useState } from 'react';
 
 import "./App.css";
 import Mark from "./components/mark/Mark";
 const Game = require("./logic/game");
 const Board = require("./logic/board");
+const game = new Game(new Board());
 
 const App = () => {
-  const game = new Game(new Board());
-  const [board, setBoard] = useState(game.board);
+  const [board, setBoard] = useState(game.board.board);
+  const [turn, setTurn] = useState(game.whoseTurnIsIt())
+  const [gameOver, setGameOver] = useState(false)
+  const [win, setWin] = useState(false)
+  const [draw, setDraw] = useState(false)
   
   const mark = (row, column, symbol) => {
     game.board.mark([row, column], symbol)
+    game.swapTurns()
+    setTurn(game.whoseTurnIsIt())
   }
+
+  useEffect(() => {
+    if (game.board.checkWins()) {
+      setGameOver(true)
+      setWin(true)
+    } else if (game.board.checkDraw()) {
+      setGameOver(true)
+      setDraw(true)
+    }
+  }, [turn])
 
   return (
     <>
       <h1>OMGHAI</h1>
-      {board.getBoard().map((row, rowIndex) => (
+      {game.board.getBoard().map((row, rowIndex) => (
         <div className="container" key={rowIndex}>
           {row.map((_, columnIndex) => (
             <Mark
               key={`${rowIndex}-${columnIndex}`}
               row={rowIndex}
               column={columnIndex}
-              symbol={"X"}
+              symbol={turn}
               callback={mark}
             />
           ))}
--- frontend/src/logic/game.js
+++ frontend/src/logic/game.js
@@ -4,10 +4,6 @@ class Game {
     this.xNext = true
   }
 
-  play = (row, column, symbol) => {
-    this.board.mark([row, column], symbol)
-  }
-
   whoseTurnIsIt = () => {
     if (this.xNext) {
       return 'X'
@@ -19,28 +15,6 @@ class Game {
   swapTurns = () => {
     this.xNext = !this.xNext
   }
-
-  // run = () => {
-  //   while (this.board.moveCount < 9) {
-  //     this.board.show()
-  //     while (true) {
-  //       if (this.board.mark([row, column], this.whoseTurnIsIt())) {
-  //         break
-  //       } else {
-  //         this.ui.occuppied()
-  //       }
-  //     }
-  //     if (this.board.checkWins()) {
-  //       this.board.show()
-  //       this.ui.victoryMessage(this.xNext)
-  //       return
-  //     } else {
-  //       this.swapTurns()
-  //       this.moveCount += 1
-  //     }
-  //   }
-  //   console.log('It is a draw!')
-  // }
 }
 
 module.exports = Game
