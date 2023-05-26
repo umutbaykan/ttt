1d20795b434a0104dc670c55ce3197c785346f49
umut <umut_baykan@hotmail.com>
2023-05-26 14:57:18 +0100

    copied over mark component from a different branch :

---
+++ frontend/src/components/mark/Mark.js
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import './Mark.css'
+
+const Mark = ({symbol, callback, row, column}) => {
+
+  const [mark, setMark] = useState(' ')
+
+  const handleMarkPress = () => {
+    if (mark === ' ') {
+    setMark(symbol) 
+    callback(row, column, symbol)
+    } else {
+      return
+    }
+  }
+
+  return (
+    <button onClick={handleMarkPress} className='mark'>{mark}</button>
+    )
+}
+
+export default Mark
